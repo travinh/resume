27393222f6492d6b393499b749c410d7c02eaab4
travinh <vinh.v.tran@ucdenver.edu>
2021-02-06 16:48:23 -0500

    add props to header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,12 @@ import React, {Component} from 'react';
 // import ReactGA from 'react-ga';
 import $ from 'jquery';
 
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 export default class App extends Component {
 
